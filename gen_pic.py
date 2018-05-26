dd205f6b83f06f92f94cfd2a84edb3669a2acf63
Brian <bcj26@case.edu>
2018-05-26 12:44:41 -0700

    Updated code to (mostly) comform to PEP8

--- gen_pic.py
+++ gen_pic.py
@@ -8,39 +8,43 @@ import argparse
 
 
 def draw_gp(n=100):
-    # Construct L as the standard five-point stencil approximation to the laplacian 
-    c = [2, -1]
+    # Construct L as the standard five-point stencil
+    # approximation to the laplacian
+    c = [2,  -1]
     c.extend([0 for x in range(n-2)])
     T = linalg.toeplitz(c)
-    L = sparse.kron(T,sparse.eye(n)) + sparse.kron(sparse.eye(n),T)
+    L = sparse.kron(T,  sparse.eye(n)) + sparse.kron(sparse.eye(n),  T)
 
-    # Solving the system Lx = w where w ~ N(0,I) <=> drawing from GP
-    w = np.random.normal(size=(n**2,1))
-    x = spsolve(L,w, use_umfpack=True)
+    # Solving the system Lx = w where w ~ N(0, I) <=> drawing from GP
+    w = np.random.normal(size=(n**2,  1))
+    x = spsolve(L,  w,  use_umfpack=True)
 
     # Reshape result
-    x = np.reshape(x,(n,n))
+    x = np.reshape(x,  (n,  n))
     return x
 
 
 def main():
-    # Parse dimension of (square) image from command line along with verbose and save flags
+    # Parse dimension of (square) image from command line
+    # along with verbose and save flags
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dim", help="Dimension of square GP image",type=int)
-    parser .add_argument("--verbose",help="Plot the gp",action="store_true")
-    parser.add_argument("--save",help="Save gp draw to .png",action="store_true")
+    parser.add_argument("--dim", 
+                help="Dimension of square GP image", type=int)
+    parser .add_argument("--verbose",
+                help="Plot the gp", action="store_true")
+    parser.add_argument("--save",
+                help="Save gp draw to .png", action="store_true")
     args = parser.parse_args()
 
     # Draw from gp
     gp = draw_gp(args.dim)
 
     # Plot and/or save image per user input
-    plot.imshow(gp,cmap='summer')
+    plot.imshow(gp, cmap='summer')
     if args.save:
-        plot.imsave('github_picture.png',gp ,cmap='summer')
+        plot.imsave('github_picture.png', gp, cmap='summer')
     if args.verbose:
         plot.show()
-
 
 
 if __name__ == "__main__":
